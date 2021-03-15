1d252df73f6d7c0c7bf5a9358decc5c779673e67
lorevange <72378630+lorevange@users.noreply.github.com>
2021-03-15 17:11:33 +0100

    Navigation Options Update
    Styling

--- navigators/MainNavigator.js
+++ navigators/MainNavigator.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 
@@ -8,11 +7,10 @@ import RecordingScreen from '../screens/RecordingScreen';
 import Colors from '../constants/Colors';
 
 const defaultStackNavOptions = {
-    headerStyle: {
-        backgroundColor: Platform.OS === 'android' ? Colors.primary : ''
-      },
-      headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primary,
-      headerTitle: 'A Screen'
+    headerTitleAlign: 'center',
+    headerTitleStyle: {
+        color: Colors.primary
+    }
 }
 
 
